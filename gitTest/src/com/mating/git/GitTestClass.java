f7df5d92a80b657d211f46495e8ef016d3a7446e
mating <mating@mating.cn>
2020-10-08 23:28:13 +0800

    修改了java文件

--- gitTest/src/com/mating/git/GitTestClass.java
+++ gitTest/src/com/mating/git/GitTestClass.java
@@ -7,6 +7,7 @@ public class GitTestClass {
         System.out.println("git....");
         System.out.println("git....");
 
+        System.out.println("修改的东西...");
 
         System.out.println(99999999);
         System.out.println(99999999);
