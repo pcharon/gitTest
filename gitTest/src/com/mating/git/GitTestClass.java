92d84adf902f876c056a59aac094e8895431ef73
mating <mating@mating.cn>
2020-10-08 22:44:02 +0800

    第一次提交java工程

---
+++ gitTest/src/com/mating/git/GitTestClass.java
@@ -0,0 +1,10 @@
+package com.mating.git;
+
+public class GitTestClass {
+
+    public static void main(String[] args) {
+        System.out.println("git....");
+        System.out.println("git....");
+        System.out.println("git....");
+    }
+}
